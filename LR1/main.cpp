370fffa7a9adcac3766fb434c88b0bdd02c1458d
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2018-10-19 00:52:18 +0300

    Update main.cpp

--- LR1/main.cpp
+++ LR1/main.cpp
@@ -20,21 +20,21 @@ int main()
 	setlocale(LC_ALL, "ru");
 	vector<Student> students;
 
-	map<string, int> tempRatingsIvan = { { "Math", 3 },{ "ИНЖГРАФ", 5 },{ "ТОЭ", 4 }, {"Политология", 2} };
-	map<string, int> tempRatingsEvgeny = { { "Math", 5 },{ "ИНЖГРАФ", 2 },{ "ТОЭ", 5 }, { "Политология", 5 } };
-	map<string, int> tempRatingsSlava = { { "Math", 4 },{ "ИНЖГРАФ", 3 },{ "ТОЭ", 5 }, { "Политология", 4 } };
-	map<string, int> tempRatingsMikhail = { { "Math", 3 },{ "ИНЖГРАФ", 5 },{ "ТОЭ", 4 }, { "Политология", 4 } };
-	map<string, int> tempRatingsSergey = { { "Math", 5 },{ "ИНЖГРАФ", 2 },{ "ТОЭ", 5 }, { "Политология", 3 } };
-	map<string, int> tempRatingsVitaly = { { "Math", 5 },{ "ИНЖГРАФ", 2 },{ "ТОЭ", 5 }, { "Политология", 3 } };
-	map<string, int> tempRatingsPetr = { { "Math", 2 },{ "ИНЖГРАФ", 2 },{ "ТОЭ", 5 }, { "Политология", 2 } };
-	map<string, int> tempRatingsVasya = { { "Math", 5 },{ "ИНЖГРАФ", 5 },{ "ТОЭ", 5 }, { "Политология", 5 } };
+	map<string, int> tempRatingsIvan = { { "Math", 3 },{ "ИНЖГРАФ", 5 },{ "ТОЭ", 4 },{ "Политология", 2 } };
+	map<string, int> tempRatingsEvgeny = { { "Math", 5 },{ "ИНЖГРАФ", 2 },{ "ТОЭ", 5 },{ "Политология", 5 } };
+	map<string, int> tempRatingsSlava = { { "Math", 4 },{ "ИНЖГРАФ", 3 },{ "ТОЭ", 5 },{ "Политология", 4 } };
+	map<string, int> tempRatingsMikhail = { { "Math", 3 },{ "ИНЖГРАФ", 5 },{ "ТОЭ", 4 },{ "Политология", 4 } };
+	map<string, int> tempRatingsSergey = { { "Math", 5 },{ "ИНЖГРАФ", 2 },{ "ТОЭ", 5 },{ "Политология", 3 } };
+	map<string, int> tempRatingsVitaly = { { "Math", 5 },{ "ИНЖГРАФ", 2 },{ "ТОЭ", 5 },{ "Политология", 3 } };
+	map<string, int> tempRatingsPetr = { { "Math", 2 },{ "ИНЖГРАФ", 2 },{ "ТОЭ", 5 },{ "Политология", 2 } };
+	map<string, int> tempRatingsVasya = { { "Math", 5 },{ "ИНЖГРАФ", 5 },{ "ТОЭ", 5 },{ "Политология", 5 } };
 	students.push_back({ "Иван", "РЛ1-11", tempRatingsIvan });
 	students.push_back({ "Евгений", "РЛ1-12", tempRatingsEvgeny });
 	students.push_back({ "Слава", "РЛ1-13", tempRatingsSlava });
 	students.push_back({ "Михаил", "ИУ8-31", tempRatingsMikhail });
 	students.push_back({ "Сергей", "ИУ8-31", tempRatingsSergey });
 	students.push_back({ "Виталий", "Э9-51", tempRatingsVitaly });
-	students.push_back({"Пётр", "Э9-55", tempRatingsPetr});
+	students.push_back({ "Пётр", "Э9-55", tempRatingsPetr });
 	students.push_back({ "Вася", "СМ5-113", tempRatingsVasya });
 
 	cout << "Задание 1" << endl;
@@ -60,7 +60,7 @@ int main()
 	cout << "Задание 3" << endl;
 	cout << count_if(students.begin(), students.end(), [](Student &s)
 	{
-		return s.ratings["Math"] == 2 || s.ratings["ИНЖГРАФ"] == 2 || 
+		return s.ratings["Math"] == 2 || s.ratings["ИНЖГРАФ"] == 2 ||
 			s.ratings["ТОЭ"] == 2 || s.ratings["Политология"] == 2;
 	});
 	cout << " студентов имеют неудовлетворительную оценку хотя бы по одному предмету" << endl;
@@ -73,6 +73,29 @@ int main()
 			s.ratings["ТОЭ"] == 5 && s.ratings["Политология"] == 5;
 	});
 	cout << " студентов сдали все экзамены на 5" << endl;
+	cout << endl;
+
+	cout << "Задание 5" << endl;
+	vector<Student> exlents;
+	copy_if(students.begin(), students.end(), back_inserter(exlents), [](Student &s)
+	{
+		return s.ratings["Math"] == 5;
+	});
+
+	for (auto el : exlents)
+	{
+		cout << el.Name << endl;
+	}
+	cout << endl;
+
+	cout << "Задание 6" << endl;
+	transform(students.begin(), students.end(), students.begin(), [](Student &s)
+	{
+		s.ratings["Политология"] = 4;
+		return s;
+	});
+
+	Print(students);
 }
 void Print(const vector<Student> &s)
 {
