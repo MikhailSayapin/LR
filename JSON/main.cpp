66bed2a779b348ba840aaf3392875d7f0a96056f
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2018-11-08 07:22:18 +0300

    Update main.cpp

--- JSON/main.cpp
+++ JSON/main.cpp
@@ -13,21 +13,4 @@ int main()
     	"city" : "Moscow",
         "street" : "Vozdvijenka"
     }
-})";
-
-TEST(Json, LoadFromString) {
-  Json object = Json::parse(json_data);
-  EXPECT_EQ(std::any_cast<std::string>(object["lastname"]), "Ivanov");
-  EXPECT_EQ(std::any_cast<bool>(object["islegal"]), false);
-  EXPECT_EQ(std::any_cast<double>(object["age"]), 25);
-
-  auto marks = std::any_cast<Json>(object["marks"]);
-  EXPECT_EQ(std::any_cast<double>(marks[0]), 4);
-  EXPECT_EQ(std::any_cast<double>(marks[1]), 5);
-
-  auto address = std::any_cast<Json>(object["address"]);
-  EXPECT_EQ(std::any_cast<std::string>(address["city"]), "Moscow");
-  EXPECT_EQ(std::any_cast<std::string>(address["street"]), "Vozdvijenka");
-}
-
 }
